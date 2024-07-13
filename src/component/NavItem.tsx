904c76f7896b2d7112026215e230b85be0c635db
Aboudebug <djarimias@gmail.com>
2024-07-14 00:02:23 +0200

    change scale

--- src/component/NavItem.tsx
+++ src/component/NavItem.tsx
@@ -6,7 +6,7 @@ export function NavItem({ title }: NavItemProps) {
     return (
         <div className="flex flex-col group">
             <p>{title}</p>
-            <p className="h-0.5 scale-0 bg-white group-hover:scale-100 transition duration-200"></p>
+            <p className="h-0.5 scale-0 bg-white group-hover:scale-x-50 transition duration-200"></p>
         </div>
     )
 }
